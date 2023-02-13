a21b10f54f3f3802652671321e6adaa85da991fc
Adam Franco <afranco@middlebury.edu>
2023-02-13 16:32:38 -0500

    Just run drush cr to test.

--- web/private/scripts/drush_config_import/drush_config_import.php
+++ web/private/scripts/drush_config_import/drush_config_import.php
@@ -1,8 +1,8 @@
 <?php
 // Import all config changes.
-echo "Importing configuration from yml files...\n";
-passthru('drush config-import -y');
-echo "Import of configuration complete.\n";
+// echo "Importing configuration from yml files...\n";
+// passthru('drush config-import -y');
+// echo "Import of configuration complete.\n";
 //Clear all cache
 echo "Rebuilding cache.\n";
 passthru('drush cr');
