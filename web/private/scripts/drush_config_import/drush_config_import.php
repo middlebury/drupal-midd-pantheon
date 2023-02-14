5bad6ab0d5053f570c232ce985279a4980bcc709
Adam Franco <afranco@middlebury.edu>
2023-02-14 17:05:40 -0500

    Uncomment drush cr from the script.

--- web/private/scripts/drush_config_import/drush_config_import.php
+++ web/private/scripts/drush_config_import/drush_config_import.php
@@ -1,9 +1,9 @@
 <?php
 // Import all config changes.
-// echo "Importing configuration from yml files...\n";
+echo "Importing configuration from yml files...\n";
 // passthru('drush config-import -y');
-// echo "Import of configuration complete.\n";
+echo "Import of configuration complete.\n";
 //Clear all cache
 echo "Rebuilding cache.\n";
-// passthru('drush cr');
+passthru('drush cr');
 echo "Rebuilding cache complete.\n";
