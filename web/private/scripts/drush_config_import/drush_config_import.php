e0ca926be0a3794bbd3b5ecd551d81540e9f379e
Adam Franco <afranco@middlebury.edu>
2023-02-14 17:16:11 -0500

    Do the config import.

--- web/private/scripts/drush_config_import/drush_config_import.php
+++ web/private/scripts/drush_config_import/drush_config_import.php
@@ -1,7 +1,7 @@
 <?php
 // Import all config changes.
 echo "Importing configuration from yml files...\n";
-// passthru('drush config-import -y');
+passthru('drush config-import -y');
 echo "Import of configuration complete.\n";
 //Clear all cache
 echo "Rebuilding cache.\n";
